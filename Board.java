cbc634ebdeb3977cd4da1c7f7820af231b03242f
Roman Ilchyshyn <romanilchyshyn@gmail.com>
2020-07-12 12:25:08 +0300

    Create empty implementations to make project compile.

---
+++ Board.java
@@ -0,0 +1,60 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class Board {
+
+    // create a board from an n-by-n array of tiles,
+    // where tiles[row][col] = tile at (row, col)
+    public Board(int[][] tiles) {
+        throw new UnsupportedOperationException();
+    }
+
+    // string representation of this board
+    public String toString() {
+        throw new UnsupportedOperationException();
+    }
+
+    // board dimension n
+    public int dimension() {
+        throw new UnsupportedOperationException();
+    }
+
+    // number of tiles out of place
+    public int hamming() {
+        throw new UnsupportedOperationException();
+    }
+
+    // sum of Manhattan distances between tiles and goal
+    public int manhattan() {
+        throw new UnsupportedOperationException();
+    }
+
+    // is this board the goal board?
+    public boolean isGoal() {
+        throw new UnsupportedOperationException();
+    }
+
+    // does this board equal y?
+    public boolean equals(Object y) {
+        throw new UnsupportedOperationException();
+    }
+
+    // all neighboring boards
+    public Iterable<Board> neighbors() {
+        throw new UnsupportedOperationException();
+    }
+
+    // a board that is obtained by exchanging any pair of tiles
+    public Board twin() {
+        throw new UnsupportedOperationException();
+    }
+
+    // unit testing (not graded)
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ Solver.java
@@ -0,0 +1,32 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class Solver {
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial) {
+        throw new UnsupportedOperationException();
+    }
+
+    // is the initial board solvable? (see below)
+    public boolean isSolvable() {
+        throw new UnsupportedOperationException();
+    }
+
+    // min number of moves to solve initial board; -1 if unsolvable
+    public int moves() {
+        throw new UnsupportedOperationException();
+    }
+
+    // sequence of boards in a shortest solution; null if unsolvable
+    public Iterable<Board> solution() {
+        throw new UnsupportedOperationException();
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+
+    }
+}
